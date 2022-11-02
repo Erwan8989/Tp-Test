bec65549b3217dca6c9aeb5d7c0bc48f8259a2be
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2022-11-02 18:10:09 +0100

    Init project

---
+++ tp_junit/junit_tp1/src/fr/emse/dsi/junit/Queue.java
@@ -0,0 +1,19 @@
+package fr.emse.dsi.junit;
+
+public class Queue<E> {
+
+	public boolean isEmpty() {
+		// TODO
+		return false;
+	}
+
+	public void push(E elem) {
+		// TODO
+	}
+
+	public E pop() {
+		// TODO
+		return null;
+	}
+
+}
---
+++ tp_junit/junit_tp1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author erwan
+ *
+ */
+module TPTest {
+}
