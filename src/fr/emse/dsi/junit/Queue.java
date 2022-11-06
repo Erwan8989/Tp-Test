03a0ca8e0fbba5873c5442ba1170273d8ae99132
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2022-11-06 15:04:04 +0100

    test emty et push (ok)

--- src/fr/emse/dsi/junit/Queue.java
+++ src/fr/emse/dsi/junit/Queue.java
@@ -1,19 +1,36 @@
 package fr.emse.dsi.junit;
-
 public class Queue<E> {
 
+	private E elem;
+
 	public boolean isEmpty() {
-		// TODO
-		return false;
+		String test = (String) this.getElem(this);
+		System.out.println(test);
+		if(test == null) {
+			return true;
+		} else {
+			return false; 
+		}
+		
+		
 	}
+	
 
 	public void push(E elem) {
-		// TODO
+		if(elem == null) {
+			this.elem = null;
+		} else {
+			this.elem = elem;
+		}
 	}
 
 	public E pop() {
 		// TODO
 		return null;
 	}
-
+	
+	public E getElem(Queue<E> queue) {
+		return this.elem;
+	}
+	
 }
